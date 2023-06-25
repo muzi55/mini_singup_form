b5609c56aaadb0ef4895a1b947e3d4c7313ca5ee
muzi55 <muzi5@kakao.com>
2023-06-25 21:23:07 +0900

    미니프로젝트 준비

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// TODO: 이 곳에 정답 코드를 작성해주세요.
+// 1. 페이지가 로드 되었을때, ID입력창에 Focusing 되어있어야함.
+// 대상: ID 입력창
+// 시점: 페이지 로드 되었을 때
+// 이벤트: focus()
+
+// $붙히는 이유 => 코드를 읽는 사람이 이거는 Dom을 가져온것이라 말함
+const $id = document.getElementById('id')
+console.log($id)
